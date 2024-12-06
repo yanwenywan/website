ee10209e8a198b495a1d6aadc786b2e2c68223cb
燕昕 <rexinkharik@gmail.com>
2024-12-06 21:27:10 +0000

    shenhaiguangji

--- shenhai/js/format.js
+++ shenhai/js/format.js
@@ -103,6 +103,7 @@ function punctuate(text) {
         .replace(/[，]/g, "、").replace(/[；：？！]/g, "。")
         .replace(/([「])/g, '<span class="speechmark start">$1</span>')
         .replace(/([」])/g, '<span class="speechmark end">$1</span>')
+        .replace(/【(.*)】/g, '<span class="black">$1</span>')
     let spanned = properJudou.replace(/([。，、；：])/g, '<span class="judou">$1</span>');
     let commented = spanned.replace(/〔(.*)〕/g, (_, p1) => {
         if (p1.length % 2 !== 0) {
