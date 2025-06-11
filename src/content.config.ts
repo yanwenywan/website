e4cde9e5a88adcad8ff7304c97d2079679c3ee2c
燕昕 <rexinkharik@gmail.com>
2025-06-11 23:15:32 +0100

    Add baoyu chapters

--- src/content.config.ts
+++ src/content.config.ts
@@ -3,5 +3,8 @@ import { z, defineCollection } from "astro:content";
 export const collections = {
     "dagengren-chapters": defineCollection({
         schema: z.object({})  // as no frontmatter
+    }),
+    "baoyu-chapters": defineCollection({
+        schema: z.object({})
     })
 }
